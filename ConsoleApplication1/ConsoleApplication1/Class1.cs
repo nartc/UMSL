8b67ea3addc0cf9215f5b17897d303a050fdad0d
nartc <ctch5@mail.umsl.edu>
2017-02-05 09:45:39 -0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Globalization;
+using nsClearConsole;
+
+namespace ConsoleApplication1
+{
+    [Serializable]
+    public class Account
+    {
+        ClearConsole myCC = new ClearConsole();
+
+        protected double balance;
+        protected int firstDate;
+        protected int secondDate;
+        protected bool usePrevDate = false;
+        //protected Calendar cal1 = new JulianCalendar();
+        //protected Calendar cal2 = new JulianCalendar();
+        protected Calendar cal1 = new GregorianCalendar();
+        protected Calendar cal2 = new GregorianCalendar();
+        protected bool dateflag = false;
+        protected double rate;
+        protected double accumInterest;
+        protected String dispDate;
+        protected String acctName;
+
+        public string Acct
+        {
+            get { return acctName; }
+        }
+
+        public Account()
+        {
+            balance = 0;
+            acctName = null;
+        }
+
+        public Account(double begin_balance, String aName)
+        {
+            balance = begin_balance;
+            acctName = aName;
+        }
+
+        public void acctMenu()
+        {
+            char input;
+
+            for (;;)
+            {
+                Console.WriteLine("Account Name: " + acctName.ToUpper());
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("a. Deposit");
+                Console.WriteLine("b. Withdraw");
+                Console.WriteLine("c. Check Balance");
+                Console.WriteLine("d. Back to Main Menu");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Please choose one: ");
+
+                String str = Convert.ToString(Console.ReadLine());
+                Thread.Sleep(1 * 200);
+                //myCC.Clear();
+                Console.Clear();
+                input = str[0];
+
+                if ((input == 'a' || input == 'A') && str.Length == 1)
+                {
+                    //getDate1();
+                    //deposit();
+                    if (dateflag)
+                    {
+                        getDate2();
+                        if (!usePrevDate)
+                        {
+                            calcInterest();
+                        }
+                        deposit();
+                        usePrevDate = false;
+                    }
+                    else
+                    {
+                        getDate1();
+                        deposit();
+                    }
+                }
+                else if ((input == 'b' || input == 'B') && str.Length == 1)
+                {
+                    //withdraw();
+                    if (dateflag)
+                    {
+                        getDate2();
+                        if (!usePrevDate)
+                        {
+                            calcInterest();
+                        }
+                        withdraw();
+                        usePrevDate = false;
+                    }
+                    else
+                    {
+                        getDate1();
+                        withdraw();
+                    }
+                }
+                else if ((input == 'c' || input == 'C') && str.Length == 1)
+                {
+                    //checkBalance();
+                    if (dateflag)
+                    {
+                        getDate2();
+                        if (!usePrevDate)
+                        {
+                            calcInterest();
+                        }
+                        if (accumInterest > 0)
+                        {
+                            Console.WriteLine("Accumulated earned interest: " + accumInterest.ToString("C2"));
+                        }
+
+                        checkBalance();
+                        usePrevDate = false;
+                    }
+                    else
+                    {
+                        getDate1();
+                        checkBalance();
+                    }
+                }
+                else if ((input == 'd' || input == 'D') && str.Length == 1)
+                {
+                    break;
+                }
+                else if (str.Length > 1)
+                {
+                    //Console.WriteLine();
+                    Console.WriteLine("Please enter only one character at a time");
+                    Console.WriteLine();
+
+                }
+                else
+                {
+                    //Console.WriteLine();
+                    Console.WriteLine("Invalid input. Please try again");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        protected void deposit()
+        {
+            Console.WriteLine("Please enter the amount you would like to deposit: ");
+            double amountD = Convert.ToDouble(Console.ReadLine());
+            balance += amountD;
+            Console.WriteLine();
+            Console.WriteLine(amountD.ToString("C2") + " was deposited into your account.");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Hit ENTER to go back...");
+            Console.ReadLine();
+            myCC.Clear();
+        }
+
+        protected void withdraw()
+        {
+            Console.WriteLine("Current balance: " + balance.ToString("C2"));
+            Console.WriteLine("Please enter the amount you would like to withdraw: ");
+            double amountW = Convert.ToDouble(Console.ReadLine());
+
+            if (amountW > balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+                Console.WriteLine();
+                withdraw();
+            }
+            else
+            {
+                balance -= amountW;
+                Console.WriteLine();
+                Console.WriteLine(amountW.ToString("C2") + " was withdrawn from your account.");
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Hit ENTER to go back...");
+                Console.ReadLine();
+                myCC.Clear();
+            }
+        }
+
+        protected void checkBalance()
+        {
+            Console.WriteLine("Current balance: " + balance.ToString("C2"));
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Hit ENTER to go back...");
+            Console.ReadLine();
+            myCC.Clear();
+        }
+
+        protected void getDate1()
+        {
+            Console.WriteLine("Please enter today's date [mm/dd/yyyy]: ");
+            String input = Convert.ToString(Console.ReadLine());
+            dispDate = input;
+            Console.WriteLine();
+
+            try
+            {
+                DateTime myDate = Convert.ToDateTime(input);
+                firstDate = cal1.GetDayOfYear(myDate);
+                dateflag = true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Invalid input.");
+                Console.WriteLine();
+                getDate1();
+
+            }
+        }
+
+        protected void getDate2()
+        {
+            bool invalidInput = false;
+            bool pastDate = false;
+
+            Console.WriteLine("Please enter today's date [mm/dd/yyyy] or 'p' to use previous date (" + dispDate + ") : ");
+            String input = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+
+            if(input.ElementAt(0) == 'p' && input.Length == 1)
+            {
+                usePrevDate = true;
+            }
+            else
+            {
+                try
+                {
+                    DateTime myDate = Convert.ToDateTime(input);
+                    secondDate = cal2.GetDayOfYear(myDate);
+
+                    if(firstDate > secondDate)
+                    {
+                        Console.WriteLine("You must enter a future date.");
+                        Console.WriteLine();
+                        pastDate = true;
+                        getDate2();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid input.");
+                    Console.WriteLine();
+                    invalidInput = true;
+                    getDate2();
+                }
+                if(input.ElementAt(0) != 'p' && invalidInput == false && pastDate == false)
+                {
+                    dispDate = input;
+                }
+            }  
+        }
+
+        protected void calcInterest()
+        {
+            int dateDiff = secondDate - firstDate;
+            rate = .05 / 365;
+            double rateTime = Math.Pow(1 + rate, dateDiff);
+            balance *= rateTime;
+            double amountInterest = balance - (balance / rateTime);
+            accumInterest += amountInterest;
+            firstDate = secondDate;
+        }
+    }
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using nsClearConsole;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+
+        List<Account> acctList = new List<Account>();
+        ClearConsole myCC = new ClearConsole();
+        string dir = @".\test.out";
+        //String aName;
+        //Account account = new Account();
+
+        public static void Main(string[] args)
+        {
+            Program myATM = new Program();
+            
+            myATM.readAccount();
+            //myATM.createAccount();
+            myATM.atmMenu();
+            myATM.writeAccount();
+        }
+
+        public void readAccount()
+        {
+            try
+            {
+                using (Stream stream = File.Open(dir, FileMode.Open))
+                {
+                    var bFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    acctList = (List<Account>)bFormatter.Deserialize(stream);
+                    stream.Close();
+                    if(acctList.Count <= 0)
+                    {
+                        createAccount();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                createAccount();
+            }
+        }
+
+        public void atmMenu()
+        {
+            int input;
+            int number;
+            string str;
+
+            do
+            {
+                myCC.Clear();
+                Console.WriteLine("\t ATM MENU");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("1. Create a new account");
+                Console.WriteLine("2. Remove an old account");
+                Console.WriteLine("3. Make a transaction");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Please choose one: ");
+                str = Console.ReadLine();
+                myCC.Clear();               
+
+                if (!int.TryParse(str, out number) || string.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("Please input [1-4] to pick an option.");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    input = Convert.ToInt32(str);
+                    if (input == 1)
+                    {
+                        createAccount();
+                        Console.WriteLine();
+                    }
+                    else if (input == 2)
+                    {
+                        removeAccount();
+                        Console.WriteLine();
+                    }
+                    else if (input == 3)
+                    {
+                        makeTransaction();
+                        Console.WriteLine();
+
+                    }
+                    else if (input == 4)
+                    {
+                        Console.WriteLine("Goodbye!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please try again.");
+                        Console.WriteLine();
+                    }
+                }
+            } while (!int.TryParse(str, out number) || string.IsNullOrEmpty(str) || Convert.ToInt32(str) != 4);
+        }
+
+        public void createAccount()
+        {
+            String aName;
+            bool accountAlreadyExists = false;
+
+            Console.WriteLine("Please enter a name for your new account: ");
+            aName = Convert.ToString(Console.ReadLine());
+            Thread.Sleep(1 * 1000);
+            myCC.Clear();
+
+
+            if (string.IsNullOrEmpty(aName))
+            {
+                Console.WriteLine("Account name is required. Please try again.");
+                Console.WriteLine();
+                createAccount();
+            }
+            else
+            {
+                for (int i = 0; i < acctList.Count; i++)
+                {
+                    if (aName.Equals(acctList[i].Acct))
+                    {
+                        accountAlreadyExists = true;
+                    }
+                }
+
+                if (accountAlreadyExists)
+                {
+                    Console.WriteLine("Sorry, there is already an account associated with this name.");
+                    Console.WriteLine("\n\n");
+                    Console.WriteLine("Hit ENTER to try again...");
+                    Console.ReadLine();
+                    myCC.Clear();
+                }
+                else
+                {
+                    acctList.Add(new Account(100.00, aName));
+                    Console.WriteLine("Account " + "|" + aName.ToUpper() + "|" + " was successfully created.");
+                    Thread.Sleep(1 * 1000);
+                    myCC.Clear();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void removeAccount()
+        {
+            int acctNumber;
+            string str;
+            String accountName;
+
+            if(acctList.Count > 0)
+            {
+                Console.WriteLine("CURRENT ACCOUNTS");
+                Console.WriteLine("----------------");
+                for(int i = 0; i < acctList.Count; i++)
+                {
+                    Console.WriteLine("Account " + (i + 1) + ": " + acctList[i].Acct.ToUpper());
+                }
+
+                Console.WriteLine("----------------");
+                Console.WriteLine("Please enter the number of the account you want to remove.");
+                str = Console.ReadLine();
+                Thread.Sleep(1 * 1000);
+                myCC.Clear();
+                int number;
+
+                if (!int.TryParse(str, out number) || string.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("Please input a number.");
+                    Console.WriteLine();
+                    removeAccount();
+                }
+                else
+                {
+                    acctNumber = Convert.ToInt32(str);
+                    if (acctNumber <= acctList.Count && acctNumber > 0)
+                    {
+                        for (int i = 0; i < acctList.Count; i++)
+                        {
+                            if (acctNumber == (i + 1))
+                            {
+                                accountName = acctList[i].Acct;
+                                acctList.RemoveAt(i);
+                                Console.WriteLine();
+                                Console.WriteLine("Account " + "|" + accountName.ToUpper() + "|" + " was successfully removed.");
+                                Thread.Sleep(1 * 1000);
+                                myCC.Clear();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no account associated with that number. Please try again.");
+                        Console.WriteLine();
+                        removeAccount();
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are currently no accounts in the system.");
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Hit ENTER to go back to main MENU...");
+                Console.ReadLine();
+                myCC.Clear();
+            }
+        }
+
+        public void makeTransaction()
+        {
+            int input;
+            string str;
+
+            if(acctList.Count > 0)
+            {
+                Console.WriteLine("CURRENT ACCOUNTS");
+                Console.WriteLine("----------------");
+                for (int i = 0; i < acctList.Count; i++)
+                {
+                    Console.WriteLine("Account " + (i + 1) + ": " + acctList[i].Acct.ToUpper());
+                }
+
+                Console.WriteLine("----------------");
+                Console.WriteLine("Please enter the account number: ");
+                str = Console.ReadLine();
+                Thread.Sleep(1 * 1000);
+                myCC.Clear();
+                int number;
+
+                if (!int.TryParse(str, out number) || string.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("Please input a number.");
+                    Console.WriteLine();
+                    makeTransaction();
+                }
+                else
+                {
+                    input = Convert.ToInt32(str);
+                    if (input <= acctList.Count && input > 0)
+                    {
+                        for (int i = 0; i < acctList.Count; i++)
+                        {
+                            if (input == (i + 1))
+                            {
+                                acctList[i].acctMenu();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no account associated with that number.");
+                        Console.WriteLine();
+                        makeTransaction();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are currently no accounts in the system.");
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Hit ENTER to go back to main MENU...");
+                Console.ReadLine();
+                myCC.Clear();
+            }
+        }
+
+        public void writeAccount()
+        {           
+            //string serializationFile = Path.Combine(dir);
+            try
+            {
+                using (Stream stream = File.Open(dir, FileMode.Create))
+                {
+                    var bFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                    bFormatter.Serialize(stream, acctList);
+                    stream.Flush();
+                    stream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
